11d8aea0eebcab19cf3136a6301c82570099794e
EC2 Default User <ec2-user@ip-172-31-37-43.ec2.internal>
2020-10-19 05:38:34 +0000

    3rd Commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
+  before_action :correct_user, only: [:destroy]
   
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.comments.new(comment_params)
-    comment.book_id = @book.id
-    comment.save
-    redirect_back(fallback_location: books_path)
+    @newbook = Book.new
+    @user = @book.user
+    @comments = @book.comments
+    @newcomment = current_user.comments.new(comment_params)
+    @newcomment.book_id = @book.id
+    if @newcomment.save
+      flash[:notice] = "successfully"
+      redirect_back(fallback_location: books_path)
+    else
+      render("books/show")
+    end
   end
 
   def destroy
@@ -18,5 +26,12 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:comment)
   end
+  
+   def correct_user
+     @comment = Comment.find(params[:id])
+     if current_user.id != @comment.user_id      
+      redirect_to books_path    
+     end
+   end
   
 end
